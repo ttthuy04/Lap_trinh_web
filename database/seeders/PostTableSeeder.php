524028a3abfed11b27fddd84974d2db5223d47b8
ttthuy04 <thuy0359255628thuy@gmail.com>
2024-12-08 18:18:18 +0700

    set up laravel

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 50; $i++) {
+            Post::create([
+                'title' => $faker->sentence(6, true),
+                'content' => $faker->paragraph(3, true)
+            ]);
+        }
+    }
+}
